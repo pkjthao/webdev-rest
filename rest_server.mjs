a46f411ba7d5fa6189f5ab9b5e780ea56400de15
Paku Thao <thao2210@stthomas.edu>
2023-11-28 16:46:57 -0600

    final updates

--- rest_server.mjs
+++ rest_server.mjs
@@ -63,6 +63,22 @@ app.get('/codes', (req, res) => {
     let params = [];
     console.log(sqlQuery);
 
+    if (req.query.hasOwnProperty('code')) {
+        let code = req.query.code.split(',');
+        console.log(code);
+        sqlQuery += ' WHERE';
+        for(let i = 0; i < code.length; i++) {
+            sqlQuery += ' code = ?';
+            params.push(code[i]);
+            if(code[i + 1] != null) {
+                sqlQuery += ' OR';
+            }
+        }
+        console.log(sqlQuery);
+        console.log(params);
+    }
+
+
     dbSelect(sqlQuery, params)
     .then((rows) => {
         let codes = '[ ';
@@ -73,7 +89,7 @@ app.get('/codes', (req, res) => {
         codes = codes.slice(0, -2);
         codes += ' ]';
         codes = JSON.parse(codes);
-        console.log(codes);
+        //console.log(codes);
         res.status(200).type('json').send(codes);
     })
     .catch((error) => {
@@ -87,6 +103,21 @@ app.get('/neighborhoods', (req, res) => {
     let sqlQuery = 'Select * FROM Neighborhoods';
     let params = [];
     console.log(sqlQuery);
+
+    if (req.query.hasOwnProperty('id')) {
+        let id = req.query.id.split(',');
+        console.log(id);
+        sqlQuery += ' WHERE';
+        for(let i = 0; i < id.length; i++) {
+            sqlQuery += ' neighborhood_number = ?';
+            params.push(id[i]);
+            if(id[i + 1] != null) {
+                sqlQuery += ' OR';
+            }
+        }
+        console.log(sqlQuery);
+        console.log(params);
+    }
 
     dbSelect(sqlQuery, params)
     .then((rows) => {
@@ -109,9 +140,86 @@ app.get('/neighborhoods', (req, res) => {
 app.get('/incidents', (req, res) => {
     //console.log(req.query); // query object (key-value pairs after the ? in the url)
     
-    let sqlQuery = 'Select * FROM Incidents LIMIT ?';
-    let params = [1000];
+    let sqlQuery = 'Select * FROM Incidents';
+    let params = [];
     console.log(sqlQuery);
+
+    
+    if(req.query.hasOwnProperty('start_date') && req.query.hasOwnProperty('end_date')) {
+        sqlQuery += ' WHERE ';
+        sqlQuery += '(';
+        sqlQuery += 'date_time BETWEEN ? AND ? ';
+        params.push(req.query.end_date);
+        params.push(req.query.start_date);
+        sqlQuery += ')';
+    }
+    if (req.query.hasOwnProperty('code')) {
+        let code = req.query.code.split(',');
+        if (req.query.hasOwnProperty('start_date')) {
+            sqlQuery += 'AND';
+        }
+        else {
+            sqlQuery += ' WHERE ';
+        }
+        //console.log(code);
+        sqlQuery += '(';
+        for(let i = 0; i < code.length; i++) {
+            sqlQuery += ' code = ?';
+            params.push(code[i]);
+            if(code[i + 1] != null) {
+                sqlQuery += ' OR';
+            }
+        }
+        sqlQuery += ')';
+    }
+    if (req.query.hasOwnProperty('grid')) {
+        let grid = req.query.grid.split(',');
+        if (req.query.hasOwnProperty('code') || req.query.hasOwnProperty('start_date')) {
+            sqlQuery += ' AND ';
+        }
+        else {
+            sqlQuery += ' WHERE ';
+        }
+        sqlQuery += '(';
+        for(let i = 0; i < grid.length; i++) {
+            sqlQuery += ' police_grid = ?';
+            params.push(grid[i]);
+            if(grid[i + 1] != null) {
+                sqlQuery += ' OR';
+            }
+        }
+        sqlQuery += ')';
+    }
+    if (req.query.hasOwnProperty('neighborhood')) {
+        let neighborhood = req.query.neighborhood.split(',');
+        if (req.query.hasOwnProperty('code') || req.query.hasOwnProperty('grid') || req.query.hasOwnProperty('start_date')) {
+            sqlQuery += ' AND ';
+        }
+        else {
+            sqlQuery += ' WHERE ';
+        }
+        sqlQuery += '(';
+        for(let i = 0; i < neighborhood.length; i++) {
+            sqlQuery += ' neighborhood_number = ?';
+            params.push(neighborhood[i]);
+            if(neighborhood[i + 1] != null) {
+                sqlQuery += ' OR';
+            }
+        }
+        sqlQuery += ')';
+    }
+    sqlQuery += ' ORDER BY date_time DESC';
+    if (req.query.hasOwnProperty('limit')) {
+        sqlQuery += ' LIMIT ?';
+        params.push(parseInt(req.query.limit));
+    }
+    else {
+        sqlQuery += ' LIMIT ?';
+        params.push(1000);
+    }
+    
+    console.log(sqlQuery);
+    console.log(params);
 
     dbSelect(sqlQuery, params)
     .then((rows) => {
@@ -127,7 +235,7 @@ app.get('/incidents', (req, res) => {
         incidents = incidents.slice(0, -2);
         incidents += ' ]';
         incidents = JSON.parse(incidents);
-        console.log(incidents);
+        //console.log(incidents);
 
         res.status(200).type('json').send(incidents);
     })
@@ -177,26 +285,26 @@ app.put('/new-incident', (req, res) => {
         });
 });
 
-// DELETE request handler for new crime incident 14172229
+// DELETE request handler for new crime incident
 app.delete('/remove-incident', (req, res) => {
-    const case_number  = req.query.case_number;
-    console.log(req.query.case_number);
+    const { case_number } = req.body;
 
     const deleteQuery = 'DELETE FROM Incidents WHERE case_number = ?';
     const deleteParams = [case_number];
 
-    dbRun(deleteQuery, deleteParams)
-        .then((result) => {
-            if (result.changes === 0) {
-                // Case number doesn't exist in the database
-                res.status(500).type('txt').send('Case number does not exist in the database');
-            } else {
-                res.status(200).type('txt').send('Incident removed successfully');
-            }
-        })
-        .catch((error) => {
-            res.status(500).type('txt').send(error);
-        });
+    db.run(deleteQuery, deleteParams, function (err) {
+        if (err) {
+            console.error('Error while deleting the incident:', err);
+            return res.status(500).type('txt').send(err.message || 'Internal Server Error');
+        }
+
+        if (this.changes === 0) {
+            return res.status(404).type('txt').send('Case number does not exist in the database');
+        }
+
+        return res.status(200).type('txt').send('Incident removed successfully');
+    });
+        
 });
 
 /********************************************************************
